8b4f1c4f956931906d3c9a2f538f17bb781177f6
LinkinPark1235 <50453828+LinkinPark1235@users.noreply.github.com>
2024-12-10 14:02:29 +0700

    sapp.rb

--- Formula/sapp.rb
+++ Formula/sapp.rb
@@ -6,11 +6,11 @@ class Sapp < Formula
 
   on_macos do
     on_arm do
-        url "https://github.com/brotherbui/public/releases/download/setapp/sapp.tgz"
+        url "https://github.com/LinkinPark1235/public/releases/download/setapp/sapp.tgz"
         sha256 "dbecfbbf5c08f3ba2eabc02dd30e90bce418687972f11f9e92da2f7dd5cb889e"
     end
     on_intel do
-        url "https://github.com/brotherbui/public/releases/download/setapp/sapp-intel.tgz"
+        url "https://github.com/LinkinPark1235/public/releases/download/setapp/sapp-intel.tgz"
         sha256 "ed9e04e6a807d10b6c7e57956a986a932bc94b61fced7a911f3154ad7145ad30"
     end
   end
